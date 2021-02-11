ff74b3348a1920b9b7e9b525fb710a4afb6d5f23
Dilan Chathuranga <dilan.hpdc00@gmail.com>
2021-02-11 20:18:46 +0530

    create Localhost PhpMyAdmin database connection using Netbeans IED. Database name - letme Table name - names attributes - id, first_name, last_name

---
+++ src/dataapp/DataApp.java
@@ -0,0 +1,72 @@
+package dataapp;
+
+import java.sql.*; // import the classes in the java.sql package
+public class DataApp {
+	public static void main (String args[]){ // to execute
+
+		System.out.println("Program Initiated");
+
+		// connecting to database
+ 		Connection con = null;
+ 		Statement stmt = null;
+
+ 	   try {
+		System.out.println("Loading the driver");
+		Class.forName("com.mysql.jdbc.Driver"); // loading the MS Access driver
+
+		System.out.println("Connecting");
+
+ 		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/letme","root","");
+		// odbc_ex is the data source name (DSN)
+
+ 		System.out.println("Connected");
+
+ 		// the transaction to be carried out - SQL statement
+ 		String queryString = "insert into Names (id, first_name, last_name) values('007','James','Bond')";
+
+		//System.out.println(queryString);
+		 stmt = con.createStatement(); // creating the statement object to work with database
+
+ 		int i = stmt.executeUpdate(queryString); // returns an integer - number of records added
+
+        	if (i != 0){// cheack wether data updating or not
+		  	 System.out.println("Data update succeded");
+		}else{
+		   	System.out.println("Data update error");
+		}
+
+		// retrieve the data in the Names table
+		queryString = "select * from Names";
+
+		ResultSet rs = stmt.executeQuery(queryString); // query result is in rs object
+
+		while (rs.next()){
+			String id = rs.getString("id"); //
+			String fname = rs.getString("first_name");
+			String lname = rs.getString("last_name");
+			System.out.println("ID = "+id+ " First Name = "+fname+" Last Name"+lname);
+			}
+			System.out.println("Thank you for using the database");
+			} 
+	   catch (SQLException e) {
+		System.out.println("Cannot find the database");
+		e.printStackTrace();
+	        }
+	   catch (ClassNotFoundException e) {
+ 		System.out.println("Cannot find the database");
+ 		}
+	   finally {
+ 		try{
+		  if(stmt != null) {
+		    stmt.close();
+		    stmt = null;
+ 		   }
+		 if(con != null) {
+		    con.close();
+		    con = null;
+		   }
+		}catch (SQLException e) { }
+
+    	          }
+	}
+}
